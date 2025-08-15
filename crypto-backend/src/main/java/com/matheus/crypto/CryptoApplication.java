93fa406413a5cdd8fedb0018e17f21a7ef936095
Matheus <mrogeriopesarini@gmail.com>
2025-08-14 21:39:15 -0300

    Initial project setup

---
+++ crypto-backend/src/main/java/com/matheus/crypto/CryptoApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApplication.class, args);
+	}
+
+}
